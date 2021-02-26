cd77e4d2ec3927b2d1e1f162bd0c46b6649ee5ca
Rz28x <76273364+Rz28x@users.noreply.github.com>
2021-02-26 13:32:52 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,15 @@
-
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title    = "TP CLICK",
+Text     = "Tap Left Alt for Tp.",
+Duration = 10,
+})
+local player = game.Players.LocalPlayer
+mouse = player:GetMouse()
+ 
+game:GetService("UserInputService").InputBegan:connect(function(inputObject, gameProcessedEvent)
+    if inputObject.KeyCode == Enum.KeyCode.LeftAlt then
+        local posi = mouse.Hit+Vector3.new(0,5,0)
+        posi = CFrame.new(posi.X,posi.Y,posi.Z)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = posi
+    end
+end)
